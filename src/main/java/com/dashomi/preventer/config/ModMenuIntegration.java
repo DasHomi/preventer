0dc0206c7808cf9ae98d8fa08dfc7c09daabb50b
DasHomi <72689274+DasHomi@users.noreply.github.com>
2023-06-07 23:04:41 +0200

    update to 1.20

--- src/main/java/com/dashomi/preventer/config/ModMenuIntegration.java
+++ src/main/java/com/dashomi/preventer/config/ModMenuIntegration.java
@@ -1,13 +1,11 @@
 package com.dashomi.preventer.config;
 
-// import com.terraformersmc.modmenu.api.ConfigScreenFactory;
-// import com.terraformersmc.modmenu.api.ModMenuApi;
-//
-// temporary disabled until mod menu is updated to 1.20
-// public class ModMenuIntegration implements ModMenuApi {
-//     @Override
-//     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-//         return CreateModConfig::createConfigScreen;
-//     }
-// }
-//
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+
+public class ModMenuIntegration implements ModMenuApi {
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return CreateModConfig::createConfigScreen;
+    }
+}
