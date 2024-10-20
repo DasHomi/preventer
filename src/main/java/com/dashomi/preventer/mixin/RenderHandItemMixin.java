d9dc98e44a7967c7f848e24d0af059cacc5b9f48
mg95 <66237187+themg95@users.noreply.github.com>
2024-10-20 15:12:52 +0300

    Rework hideShield and hideTotem in to one option

--- src/main/java/com/dashomi/preventer/mixin/RenderHandItemMixin.java
+++ src/main/java/com/dashomi/preventer/mixin/RenderHandItemMixin.java
@@ -10,7 +10,7 @@ import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
+import net.minecraft.registry.Registries;
 import net.minecraft.util.Hand;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -25,18 +25,18 @@ public class RenderHandItemMixin {
             cancellable = true
     )
     private void hideOffhandItem(LivingEntity entity, ItemStack stack, ModelTransformationMode renderMode, boolean leftHanded, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo callbackInfo) {
+        if (!PreventerClient.config.hideOffhand) return;
+
         if (!MinecraftClient.getInstance().options.getPerspective().isFirstPerson() ||
-                stack.isEmpty() || entity != MinecraftClient.getInstance().player || (entity.getActiveHand().equals(Hand.OFF_HAND) && entity.isUsingItem())) return;
+                stack.isEmpty() || entity != MinecraftClient.getInstance().player) return;
+
+        if (PreventerClient.config.unhideOffhandOnUse && (entity.getActiveHand().equals(Hand.OFF_HAND) && entity.isUsingItem())) return;
 
         ClientPlayerEntity player = MinecraftClient.getInstance().player;
         if (player != null && player.getOffHandStack() != stack) return;
         Item handItem = stack.getItem();
 
-        if (handItem == Items.SHIELD && PreventerClient.config.hideShield) {
-            callbackInfo.cancel();
-        }
-
-        if (handItem == Items.TOTEM_OF_UNDYING && PreventerClient.config.hideTotem) {
+        if (PreventerClient.config.hiddenOffhandItems.contains(Registries.ITEM.getId(handItem).toString())) {
             callbackInfo.cancel();
         }
     }
