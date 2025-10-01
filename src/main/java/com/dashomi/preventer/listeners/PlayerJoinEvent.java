bd87130fd44f96c20718bafccd60688983a1ede2
DasHomi <72689274+DasHomi@users.noreply.github.com>
2025-10-01 21:57:51 +0200

    move welcome message to SystemToast

--- src/main/java/com/dashomi/preventer/listeners/PlayerJoinEvent.java
+++ src/main/java/com/dashomi/preventer/listeners/PlayerJoinEvent.java
@@ -5,13 +5,14 @@ import com.dashomi.preventer.config.PreventerConfig;
 import net.fabricmc.fabric.api.networking.v1.PacketSender;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.client.toast.SystemToast;
 import net.minecraft.text.Text;
 
 public class PlayerJoinEvent {
     public static void playerJoinListener(ClientPlayNetworkHandler handler, PacketSender sender, MinecraftClient client) {
         if(PreventerClient.config.welcomeMessage) {
             assert client.player != null;
-            client.player.sendMessage(Text.translatable("text.preventer.welcomeMessage"), false);
+            client.getToastManager().add(SystemToast.create(client, SystemToast.Type.UNSECURE_SERVER_WARNING, Text.translatable("text.preventer.welcomeToastTitle"), Text.translatable("text.preventer.welcomeToast")));
             PreventerClient.config.welcomeMessage = false;
             PreventerConfig.save();
         }
